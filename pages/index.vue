21b4ed504f3dc5c10e93083c5bda2e846adcc9f2
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2022-07-12 13:57:50 +0300

    v1.0.7 - index route

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <div></div>
+  <div class="logo_container">
+    <img class="logo" src="../assets/images/icon_1.png" alt="dnd_logo" />
+  </div>
 </template>
+<style scoped>
+.logo_container {
+  widows: 100vw;
+  height: 100vh;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  max-height: 80vh;
+  max-width: 80vw;
+}
+</style>
